4cfbe4f2b1889f34f034479d41ce0bb5fb6901b4
Jordi Mas <jmas@softcatala.org>
2021-12-26 19:39:44 +0100

    Check version

---
+++ rebost-links.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Jordi Mas i Hernandez <jmas@softcatala.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+
+import xml.etree.ElementTree as ET
+import logging
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+def check_link(url):
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        x =  urlopen(req)
+        return 200
+    except HTTPError as e:
+        return e.code
+    
+    except Exception as e:
+        return 523
+
+def check_links(source_filename):
+    tree = ET.parse(source_filename)
+    root = tree.getroot()
+
+    items = []
+    words = 0
+    for entry in root.iter('item'):
+        #print(f"tag: {entry.tag}")
+        json_item = {}
+        publish = False
+        for item in entry:
+            #print(item.tag)
+        
+            url = None
+            if item.tag == "{http://wordpress.org/export/1.2/}status":
+                if item.text == "publish":
+                    publish = True
+
+            if item.tag == "title":
+                json_item['title'] = item.text
+
+            if item.tag == "{http://wordpress.org/export/1.2/}postmeta":
+                #print("aqui")
+                link = False
+                for child_item in item:
+                    #print(child_item.tag)
+                    if child_item.tag == '{http://wordpress.org/export/1.2/}meta_key':
+                        #print(child_item.text)
+                        if 'lloc_web_programa' == child_item.text:
+                            link = True
+
+                    if link and child_item.tag == '{http://wordpress.org/export/1.2/}meta_value':
+                        #print(child_item.text)
+                        link = False
+                        url = child_item.text
+
+            if url is not None:    
+                result = check_link(url)
+                logging.debug(f"Checked {url} status code: {result}")
+                if result != 200:
+                    print(f"Programa {json_item['title']} - link {child_item.text}, error {result} ")
+
+         
+  
+    print(f"Processed {len(items)} items")
+
+
+def main():
+    print("Converts a WordPress export to a JSON usable dataset")
+    logging.basicConfig(filename="rebost-links.log", level=logging.DEBUG, filemode="w")
+    check_links("raw/programes.xml")
+
+if __name__ == "__main__":
+    main()
